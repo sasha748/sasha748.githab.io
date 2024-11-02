faf23e835d840d5531bd78d78fcd1f4f0b96b19f
sasha748 <aleksandragerbova14@gmail.com>
2024-11-02 21:26:08 +0300

    Add files via upload

---
+++ app/scripts/js-script.js
@@ -0,0 +1,20 @@
+
+
+var h2Showed = false;
+
+
+function changeDisplay() {
+    const header3 = document.getElementById("1") ;
+    header3.style.display=h2Showed ? 'block' : 'none';
+    h2Showed = !h2Showed;
+}
+
+
+
+
+
+
+
+
+
+
